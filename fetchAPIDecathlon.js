b756296e2d391f1604b32de3325810e39c8d7b14
thierrypailhas <53034645+thierrypailhas@users.noreply.github.com>
2019-10-23 16:16:28 +0200

    Add files via upload

--- fetchAPIDecathlon.js
+++ fetchAPIDecathlon.js
@@ -4,17 +4,20 @@ fetch('https://sports.api.decathlon.com/sports/recommendations/geolocation?coord
 .then(response => response.json())
 .then(data => {
   console.log(data) // Prints result from `response.json()` in getRequest
-    //var sports = JSON.parse(data);
     data.forEach(function (article) {
         // Ajout du titre et du contenu de chaque article
-        var titreElt = document.createElement("h2");
+        var titreElt = document.createElement("h1");
         titreElt.textContent = article.id;
-        var contenuElt = document.createElement("p");
+        var contenuElt = document.createElement("div");
         contenuElt.textContent = article.attributes.name;
 		var descriptionElt = document.createElement("p");
 		descriptionElt.textContent = article.attributes.description	;
-		var iconElt = document.createElement("p");
-		iconElt.textContent = article.attributes.icon	;
+		var iconElt = document.createElement("img");
+		//iconElt.textContent = article.attributes.icon;
+		iconElt.setAttribute('src', article.attributes.icon);
+		iconElt.setAttribute('alt', 'na');
+		iconElt.setAttribute('height', '50px');
+		iconElt.setAttribute('width', '50px');
 		
         //sportElt.appendChild(titreElt);
         sportElt.appendChild(contenuElt);
